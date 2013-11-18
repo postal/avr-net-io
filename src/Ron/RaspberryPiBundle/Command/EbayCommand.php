0999d95e0325c5333185db511316970ea28ac739
postal <siedler1@freenet.de>
2013-11-18 21:41:47 +0100

    Merge branch 'umbau-pir'
    Conflicts:
    	src/Ron/ConsumptionBundle/Resources/views/Consumption/index.html.twig

--- src/Ron/RaspberryPiBundle/Command/EbayCommand.php
+++ src/Ron/RaspberryPiBundle/Command/EbayCommand.php
@@ -10,17 +10,18 @@ namespace Ron\RaspberryPiBundle\Command;
 
 
 use Goutte\Client;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\Input;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\CssSelector\Node\ElementNode;
 use Symfony\Component\DomCrawler\Crawler;
 
-class EbayCommand extends Command
+class EbayCommand extends ContainerAwareCommand
 {
     protected $url = 'http://kleinanzeigen.ebay.de/anzeigen/s-suchanfrage.html?keywords=hochbett+paidi&categoryId=&locationStr=Berlin&locationId=3331&radius=50&sortingField=SORTING_DATE&adType=&posterType=&pageNum=1&action=find&maxPrice=&minPrice=';
+
+    public $filename = '/home/ron/last_search_result.txt';
+    protected $mail = 'siedler1@freenet.de';
 
     protected function configure()
     {
@@ -43,35 +44,80 @@ class EbayCommand extends Command
         #    $content = unserialize($memcache->get('content'));
         #}
 
-        #echo $content;
-        #$crawler = new Crawler();
-        #$crawler->addHtmlContent($content);
-        # echo $crawler->html();exit;
+        $i = 1;
+        $content = "";
         foreach ($crawler->filterXPath('//table[@id="srchrslt-adtable"]/child::tr') as $rowNode) {
             /** @var $rowNode \DOMElement */
 
             $lineText = array();
             $crawler = new Crawler();
             $crawler->addNode($rowNode);
-            #echo  $description = $crawler->filterXPath('//td[@class="c-td-adtble-time"]')->html();
-            var_dump($crawler->filter('.c-td-adtble-dscr.h-td-first p'));
-            if ($description = $crawler->filter('.c-td-adtble-dscr.h-td-first p')) {
-                echo $description->text();
+            $description = $crawler->filter('.c-td-adtble-dscr p');
+            if (0 == $description->count()) {
+                $description = '';
+            } else {
+                $description = $description->text();
             }
-            echo $pice = $crawler->filter('.price')->html();
-            echo $location = str_replace('<br>', ' ', ($crawler->filter('h3.c-h-adtble-lctn')->html()));
-            echo $title = $crawler->filter('a.ad-title')->html();
-            #echo  $description = $crawler->filterXPath('//td[@class="c-td-adtble-dscr "]')->text();
-            #  $crawler->first();
-            /** @var $td \DOMElement */
-            # foreach($rowNode->childNodes as $td){
-            #  #      echo "huhu";
-            #     $lineText[] = $td->textContent;
-            # }
-            #   var_dump($lineText);
-            #      $output->writeln(implode(' | ', $lineText));
+            $price = $crawler->filter('.price');
+            if (0 == $price->count()) {
+                $price = '';
+            } else {
+                $price = $price->text();
 
+            }
+            $location = str_replace('<br>', ' ', ($crawler->filter('h3.c-h-adtble-lctn')->html()));
+            $title = $crawler->filter('a.ad-title')->text();
+            $link = $crawler->filter('.c-td-adtble-dscr a.ad-title')->attr('href');
+            $date = $crawler->filter('td.c-td-adtble-time')->text();
+            $content .= $i . '. ' . $title . '|' . $description . '|' . $price . '|' . $location;
+
+            $emailContent[] = array(
+                'title' => $title,
+                'description' => $description,
+                'price' => $price,
+                'location' => $location,
+                'link' => $link,
+                'date' => $date,
+            );
+
+            $output->writeln($i . '. ' . $title . '|' . $description . '|' . $price . '|' . $location);
+            $i++;
         }
-        #$output->writeln($crawler->filterXPath('//table[id="srchrslt-adtable"]')->html());
+        $lastContent = file_get_contents($this->filename);
+
+
+        if ($lastContent != base64_encode($content)) {
+            $output->writeln('Angebote haben sich geändert.');
+            file_put_contents($this->filename, base64_encode($content));
+            $message = $this->getMessage($emailContent);
+            $mailer = $this->getContainer()->get('mailer');
+            $mailer->send($message);
+
+            $output->writeln('Eine E-Mail wurde versandt.');
+            $spool = $mailer->getTransport()->getSpool();
+            $transport = $this->getContainer()->get('swiftmailer.transport.real');
+
+            $spool->flushQueue($transport);
+        } else {
+            $output->writeln('Die Angebote haben sich nicht geändert.');
+        }
+    }
+
+    private function getMessage($data)
+    {
+        $message = new \Swift_Message();
+        $message->setSubject('Neues Angebot');
+        $message->setContentType('text/html');
+        $message->setTo($this->mail);
+        $message->setBody($this->getBody($data));
+        $message->addFrom('keller.servebeer@gmail.com');
+        return $message;
+    }
+
+    protected function getBody($data)
+    {
+        $templating = $this->getContainer()->get('templating');
+        $body = $templating->render('RonRaspberryPiBundle:Ebay:ebay_mail.html.twig', array('data' => $data));
+        return $body;
     }
 } 
