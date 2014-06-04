6bc1bd6598be9ef3444a6897884e0bc148709e35
postal <siedler1@freenet.de>
2014-06-04 08:17:29 +0200

    change deployment config

---
+++ app/config/deploy.rb
@@ -0,0 +1,21 @@
+set   :application,   "Raspberry Pi"
+set   :deploy_to,     "/var/www/raspberry-pi"
+set   :domain,        "192.168.178.25"
+
+set   :scm,           :git
+set   :repository,    "git@github.com:postal/avr-net-io.git"
+
+role  :web,           domain
+role  :app,           domain, :primary => true
+
+set   :use_sudo,      true
+set   :keep_releases, 3
+set :use_composer, true
+set :update_vendors, true
+set :shared_files,      ["app/config/parameters.yml"]
+set :writable_dirs,       ["app/cache", "app/logs", "web/bundles"]
+set :webserver_user,      "www-data"
+set :permission_method,   :acl
+set :use_set_permissions, true
+#set   :deploy_via,    :copy
+#ssh_options[:port] = "444"
